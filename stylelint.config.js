bd1a28780ac0d71d256584eb731baf6cdbcb0775
Margarita Kaerlieva <margaritaaverckina@yandex.ru>
2024-01-28 19:05:51 +0300

    Проект посмотри в окно

---
+++ stylelint.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    rules: {
+     'no-duplicate-selectors': true,
+     'color-no-invalid-hex': true,
+     'property-no-unknown': true,
+     'value-keyword-case': ['lower', { camelCaseSvgKeywords: true }],
+     'declaration-block-no-duplicate-properties': true,
+     'declaration-block-no-shorthand-property-overrides': true,
+     'comment-no-empty': true,
+     'no-duplicate-selectors': true,
+     'no-invalid-position-at-import-rule': true,
+     'alpha-value-notation': 'number',
+     'color-function-notation': 'modern',
+     'color-hex-length': 'short',
+     'length-zero-no-unit': true,
+     'font-family-name-quotes': 'always-where-required',
+     'function-url-quotes': 'never',
+     'import-notation': 'url',
+     'declaration-no-important': true,
+     'declaration-block-single-line-max-declarations': 1,
+     'no-unknown-animations': true,
+     'selector-type-case': 'lower',
+     'rule-empty-line-before': 'always',
+     'at-rule-empty-line-before': 'always',
+     'selector-class-pattern': null,
+     'font-family-name-quotes': 'always-where-recommended',
+    }
+  }
+  
